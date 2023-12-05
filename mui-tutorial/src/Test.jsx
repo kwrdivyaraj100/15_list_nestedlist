46b3c8fb6a19f0d058276e61c28389295ae4ed6a
Divya R <kwrdivyaraj100@gmail.com>
2023-12-05 15:27:02 +0530

    Added expandable list with state management

--- mui-tutorial/src/Test.jsx
+++ mui-tutorial/src/Test.jsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Box, Collapse, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
 
-
+const array = ["First", "Second", "Third", "Fourth", "Fifth"]
 function Test() {
+    const [open, setOpen] = useState(false);
     return (
-        <div>Test</div>
+        <Box>
+            <List>
+                <ListItem divider>
+                    <ListItemButton onClick={() => setOpen(true)}>
+                        <ListItemIcon>{">"}</ListItemIcon>
+                        <ListItemText primary={"Expand List"} />
+                    </ListItemButton>
+                </ListItem>
+            </List>
+            <Collapse in={open}>
+                <List sx={{ marginLeft: 25 }}>
+                    {array.map((listElm) => (
+                        <ListItem divider>
+                            <ListItemButton onClick={() => setOpen(false)}>
+                                <ListItemText primary={listElm} />
+                            </ListItemButton>
+                        </ListItem>
+                    ))}
+                </List>
+            </Collapse>
+
+        </Box>
     )
+
 }
 
 export default Test;
